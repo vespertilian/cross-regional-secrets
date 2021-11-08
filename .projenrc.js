14b3fe1118f67418abeb72c073e3c46f9a6cf7b7
Pahud Hsieh <pahudnet@gmail.com>
2021-11-08 09:23:53 +0000

    support cdk-remote-stack

--- .projenrc.js
+++ .projenrc.js
@@ -5,6 +5,10 @@ const project = new AwsCdkTypeScriptApp({
   name: 'cross-regional-secrets',
   cdkDependencies: [
     '@aws-cdk/aws-secretsmanager',
+    '@aws-cdk/aws-ssm',
+  ],
+  deps: [
+    'cdk-remote-stack',
   ],
   devDeps: [
     '@aws-cdk/core',
--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,54 @@
 import { App, CfnOutput, Construct, Stack, StackProps } from '@aws-cdk/core';
 import * as secrets from '@aws-cdk/aws-secretsmanager'
+import * as ssm from '@aws-cdk/aws-ssm';
+import { RemoteParameters } from 'cdk-remote-stack';
 
 /**
  * This stack creates the secrets in Primary region
  */
 export class SecretStack extends Stack {
-  readonly secretFullArn: string;
+  readonly secretName: string;
   readonly secret: secrets.Secret;
   constructor(scope: Construct, id: string, props: StackProps = {}) {
     super(scope, id, props);
 
-    const secretName = 'AuthToken'
+    this.secretName = 'AuthToken'
 
     this.secret = new secrets.Secret(this, 'MySecret', {
-      secretName,
+      secretName: this.secretName
     })
     this.secret.addReplicaRegion('us-east-1');
-    this.secretFullArn = this.secret.secretArn
+
+    // write into the ssm parameter store
+    const parameter = new ssm.StringParameter(this, 'secretParameter', {
+      parameterName: `/${Stack.of(this).stackName}/${this.secretName}`,
+      stringValue: this.secret.secretArn,
+    });
   
     new CfnOutput(this, 'SecretArnOutput', { value: this.secret.secretArn })
     new CfnOutput(this, 'SecretNameOutput', { value: this.secret.secretName })
     new CfnOutput(this, 'SecretFullArnOutput', { value: this.secret.secretFullArn! })
+    new CfnOutput(this, 'SecretParameterName', { value: parameter.parameterName})
     // define resources here...
   }
 }
 
-/**
- * The App stack. 
- */
 export interface AppStackProps extends StackProps {
-  readonly secretFullArn: string;
+  readonly secretStack: SecretStack;
 }
 
 export class AppStack extends Stack {
   constructor(scope: Construct, id: string, props: AppStackProps) {
     super(scope, id, props);
 
-    console.log('importing', props.secretFullArn)
-    const importedSecret = secrets.Secret.fromSecretCompleteArn(this, 'ImportedSecret', props.secretFullArn)
+    const remoteParameters = new RemoteParameters(this, 'RemoteParam', {
+      path: `/${props.secretStack.stackName}`,
+      region: `${props.secretStack.region}`,
+    })
+    const stackName = props.secretStack.stackName;
+    const secretName = props.secretStack.secretName;
+    const secretFullArn = remoteParameters.get(`/${stackName}/${secretName}`);
+    const importedSecret = secrets.Secret.fromSecretCompleteArn(this, 'ImportedSecret', secretFullArn)
 
     new CfnOutput(this, 'importedSecretArnOutput', { value: importedSecret.secretArn })
   }
@@ -65,13 +76,13 @@ const secretStack = new SecretStack(app, 'my-secret-stack', {env: secretEnv } )
 // create the app stack in JP
 new AppStack(app, 'my-app-stack-jp', { 
   env: appJPEnv,
-  secretFullArn: secretStack.secretFullArn,
+  secretStack: secretStack,
 });
 
 // create the app stack in US
 new AppStack(app, 'my-app-stack-us', { 
   env: appUSEnv,
-  secretFullArn: secretStack.secretFullArn,
+  secretStack: secretStack,
 });
 
 app.synth();
