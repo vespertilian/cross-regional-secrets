6372f458c1643246f183dab18cfb4247992d6cdd
Chris Yang <kimisme9386@gmail.com>
2021-11-08 11:16:41 +0000

    feat:　ａｄｄ　ｇｉｔｐｏｄ

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,4 @@
-const { AwsCdkTypeScriptApp } = require('projen');
+const { AwsCdkTypeScriptApp, Gitpod } = require('projen');
 const project = new AwsCdkTypeScriptApp({
   cdkVersion: '1.95.2',
   defaultReleaseBranch: 'main',
@@ -20,6 +20,23 @@ project.package.addField('resolutions', {
   'set-value': '^4.0.1',
   'ansi-regex': '^5.0.1',
 });
+
+let gitpod = new Gitpod(project, {
+  prebuilds: {
+    addCheck: true,
+    addBadge: true,
+    addLabel: true,
+    branches: true,
+    pullRequests: true,
+    pullRequestsFromForks: true,
+  },
+});
+
+/* spellchecker: disable */
+gitpod.addVscodeExtensions(
+  'https://github.com/microsoft/vscode-eslint/releases/download/release%2F2.2.20-Insider/vscode-eslint-2.2.0.vsix',
+);
+
 
 
 project.synth();
