1133c91fd570862de9609d77b31dccbef9036ad3
Pahud Hsieh <pahudnet@gmail.com>
2021-11-08 04:43:53 +0000

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,9 @@
+const { AwsCdkTypeScriptApp } = require('projen');
+const project = new AwsCdkTypeScriptApp({
+  cdkVersion: '1.95.2',
+  defaultReleaseBranch: 'main',
+  name: 'cross-regional-secrets',
+
+
+});
+project.synth();
